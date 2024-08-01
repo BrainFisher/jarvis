efed18bbd7fa1654ebd4cad829eeb84e2ee46d80
BrainFisher <164922601+BrainFisher@users.noreply.github.com>
2024-08-01 14:45:24 +0300

    чистка

--- jarvis/core/views.py
+++ jarvis/core/views.py
@@ -51,39 +51,17 @@ def profile(request):
     else:
         profile_form = ProfileForm(instance=request.user.profile)
 
-    return render(request, 'profile.html', {
-        'profile_form': profile_form,
-    })
-
+    return render(request, 'profile.html', {'profile_form': profile_form})
 
 
 def game(request):
     return render(request, 'game.html')
 
+
 def get_battery_status(request):
     battery_level, battery_status = get_battery_info()
-    return JsonResponse({
-        'battery_level': battery_level,
-        'battery_status': battery_status
-    })
+    return JsonResponse({'battery_level': battery_level, 'battery_status': battery_status})
 
-
-
-# def battery_status_view(request):
-#     battery_level, battery_status = get_battery_info()
-    
-#     context = {
-#         'battery_level': battery_level,
-#         'battery_status': battery_status,
-#     }
-#     return render(request, 'battery_status.html', context)
-
-
-# def news_view(request):
-#     rss_url = 'https://www.liga.net/news/all/rss.xml'
-#     news_items = fetch_rss_feed(rss_url)
-#     return render(request, 'news.html', {'news_items': news_items})
-#
 
 def chat_view(request):
     response = None
@@ -95,12 +73,10 @@ def chat_view(request):
 
 @login_required
 def home_view(request):
-    """
-    Renders the home page with tasks due today and birthdays today.
-    """
     today = date.today()
     tasks_today = Task.objects.filter(due_date=today, owner=request.user)
-    birthdays_today = Contact.objects.filter(birthday__month=today.month, birthday__day=today.day, user=request.user)
+    birthdays_today = Contact.objects.filter(
+        birthday__month=today.month, birthday__day=today.day, user=request.user)
 
     context = {
         'tasks_today': tasks_today,
@@ -110,18 +86,6 @@ def home_view(request):
 
 
 def register_view(request):
-    """
-    Handles the user registration process. If the request method is POST, it will
-    validate the form and create a new user. If the form is valid, the user is logged
-    in and redirected to the home page. If the request method is GET, it will render
-    an empty registration form.
-
-    Args:
-        request (HttpRequest): The request object used to generate this response.
-
-    Returns:
-        HttpResponse: The rendered registration page with the form.
-    """
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -134,25 +98,13 @@ def register_view(request):
 
 
 def login_view(request):
-    """
-    Handles the user login process. If the request method is POST, it will validate
-    the form and authenticate the user. If the credentials are valid, the user is logged
-    in and redirected to the home page. If the request method is GET, it will render
-    an empty login form.
-
-    Args:
-        request (HttpRequest): The request object used to generate this response.
-
-    Returns:
-        HttpResponse: The rendered login page with the form.
-    """
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
-            if user is not None:
+            if user:
                 login(request, user)
                 return redirect('home')
     else:
@@ -162,16 +114,6 @@ def login_view(request):
 
 @require_POST
 def logout_view(request):
-    """
-    Handles the user logout process. This view requires a POST request for security
-    reasons. Upon logging out, the user is redirected to the home page.
-
-    Args:
-        request (HttpRequest): The request object used to generate this response.
-
-    Returns:
-        HttpResponse: A redirect to the home page.
-    """
     logout(request)
     return redirect('home')
 
@@ -179,51 +121,37 @@ def logout_view(request):
 @login_required
 def profile_view(request):
     if request.method == 'POST':
-        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user)
+        profile_form = ProfileForm(
+            request.POST, request.FILES, instance=request.user.profile)
         if profile_form.is_valid():
             profile_form.save()
             return redirect('profile')
     else:
-        profile_form = ProfileForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
 
     password_form = CustomPasswordChangeForm(user=request.user)
-
-    return render(request, 'profile.html', {
-        'profile_form': profile_form,
-        'password_form': password_form,
-    })
+    return render(request, 'profile.html', {'profile_form': profile_form, 'password_form': password_form})
 
 
 @login_required
 def change_password_view(request):
     if request.method == 'POST':
-        password_form = CustomPasswordChangeForm(data=request.POST, user=request.user)
+        password_form = CustomPasswordChangeForm(
+            data=request.POST, user=request.user)
         if password_form.is_valid():
             user = password_form.save()
-            update_session_auth_hash(request, user)  # Important!
+            update_session_auth_hash(request, user)
             return redirect('profile')
     else:
         password_form = CustomPasswordChangeForm(user=request.user)
 
     return render(request, 'profile.html', {
         'password_form': password_form,
-        'profile_form': ProfileForm(instance=request.user),
+        'profile_form': ProfileForm(instance=request.user.profile),
     })
 
 
 class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
-    """
-    Handles the password reset process. This view sends an email to the user with instructions
-    to reset their password.
-
-    Attributes:
-        template_name (str): The template to render for the password reset form.
-        email_template_name (str): The template to use for the password reset email.
-        html_email_template_name (str): The HTML template to use for the password reset email.
-        success_url (str): The URL to redirect to after successfully sending the password reset email.
-        success_message (str): The message to display upon successful email sending.
-        subject_template_name (str): The template for the email subject.
-    """
     template_name = 'password_reset.html'
     email_template_name = 'password_reset_email.html'
     html_email_template_name = 'password_reset_email.html'
@@ -232,31 +160,8 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
     subject_template_name = 'password_reset_subject.txt'
 
 
-# @login_required
-# def contact_list_view(request):
-#     """
-#     Renders the contact list page. This view requires the user to be logged in.
-#
-#     Args:
-#         request (HttpRequest): The request object used to generate this response.
-#
-#     Returns:
-#         HttpResponse: The rendered contact list page.
-#     """
-#     return render(request, 'contact_list.html')
-
 @login_required
 def file_list_view(request):
-    """
-    Renders the file list page with the ability to filter files by name and category.
-    This view requires the user to be logged in.
-
-    Args:
-        request (HttpRequest): The request object used to generate this response.
-
-    Returns:
-        HttpResponse: The rendered file list page with the files and statistics.
-    """
     query = request.GET.get('q', '')
     category = request.GET.get('category', '')
     files = File.objects.filter(user=request.user)
@@ -297,18 +202,6 @@ def file_list_view(request):
 
 @login_required
 def upload_file_view(request):
-    """
-    Handles the file upload process. If the request method is POST, it will validate
-    the form and save the uploaded file. If the form is valid, the file is saved and
-    the user is redirected to the file list page. If the request method is GET, it will
-    render an empty file upload form.
-
-    Args:
-        request (HttpRequest): The request object used to generate this response.
-
-    Returns:
-        HttpResponse: The rendered file upload page with the form.
-    """
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         form.instance.user = request.user
@@ -323,8 +216,7 @@ def upload_file_view(request):
                         file_instance.category = 'image'
                     elif mime_type.startswith('video'):
                         file_instance.category = 'video'
-                    elif mime_type in ['application/pdf', 'application/msword',
-                                       'application/vnd.openxmlformats-officedocument.wordprocessingml.document']:
+                    elif mime_type in ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']:
                         file_instance.category = 'document'
                     else:
                         file_instance.category = 'other'
@@ -342,17 +234,6 @@ def upload_file_view(request):
 
 @login_required
 def delete_file_view(request, file_id):
-    """
-    Handles the file deletion process. This view requires the user to be logged in.
-    If the request method is POST, it will delete the specified file.
-
-    Args:
-        request (HttpRequest): The request object used to generate this response.
-        file_id (int): The ID of the file to be deleted.
-
-    Returns:
-        HttpResponse: A redirect to the file list page after successful deletion.
-    """
     file = get_object_or_404(File, id=file_id, user=request.user)
     if request.method == 'POST':
         file.delete()
@@ -360,48 +241,14 @@ def delete_file_view(request, file_id):
     return render(request, 'confirm_delete.html', {'file': file})
 
 
-# @login_required
-# def news_view(request):
-#     """
-#     Renders the news page. This view requires the user to be logged in.
-
-#     Args:
-#         request (HttpRequest): The request object used to generate this response.
-
-#     Returns:
-#         HttpResponse: The rendered news page.
-#     """
-#     return render(request, 'news.html')
-
-
 @login_required
 def user_list_view(request):
-    """
-    Renders the user list page. This view requires the user to be logged in.
-
-    Args:
-        request (HttpRequest): The request object used to generate this response.
-
-    Returns:
-        HttpResponse: The rendered user list page with the list of users.
-    """
     users = CustomUser.objects.all()
     return render(request, 'user_list.html', {'users': users})
 
 
 @login_required
 def delete_user_view(request, user_id):
-    """
-    Handles the user deletion process. This view requires the user to be logged in.
-    If the request method is POST, it will delete the specified user.
-
-    Args:
-        request (HttpRequest): The request object used to generate this response.
-        user_id (int): The ID of the user to be deleted.
-
-    Returns:
-        HttpResponse: A redirect to the user list page after successful deletion.
-    """
     user = get_object_or_404(CustomUser, id=user_id)
     user.delete()
     return redirect('user_list')
